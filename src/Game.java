48acb2089798085d6f920a42ad8589604f17a592
DevinDrd <ddarnel8@msudenver.edu>
2021-01-31 14:50:17 -0700

    Progress on Map class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,8 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+
+import java.io.FileNotFoundException;
+
 public class Game {
 
     private Window window;
@@ -7,18 +12,39 @@ public class Game {
 
     private boolean RENDER_TIME = false; // display ups and fps to standard output
 
-    private Map map;
+	private Map map;
+	private String mapPath;
 
     public Game() {
         window = new Window();
 
         ups = 60;
-        fps = 60;
+		fps = 60;
+		
+		mapPath = "./res/maps/map.txt";
 
         init();
     }
 
     private void init() {
+		try {
+			map = new Map(mapPath);
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not file: " + mapPath);
+			System.exit(0);
+		} catch (InputMismatchException e) {
+			System.out.println("Unable to pars file: " + mapPath);
+			System.exit(0);
+		} catch (NoSuchElementException e) {
+			System.out.println("Unable to pars file: " + mapPath);
+			System.exit(0);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (NullPointerException e) {
+			System.out.println("Illegal path: " + mapPath);
+			System.exit(0);
+		}
         
     }
 
--- src/Map.java
+++ src/Map.java
@@ -1,3 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Map {
+
+    private ArrayList<Object> objects;
+
+    public Map() {
+        objects = new ArrayList<Object>();
+    }
+
+    public Map(String pth) throws FileNotFoundException {
+        loadMap(pth);
+    }
+
+    public void loadMap(String path) throws FileNotFoundException {
+        Scanner file = new Scanner(new File(path));
+
+        objects = new ArrayList<Object>();
+
+        while(file.hasNextLine()) {
+            String type = file.nextLine();
+
+            if (type.equals("triangle")) {
+                loadTriangle(file);
+            }
+            else {
+                System.out.println("Unknown type of object \"" + type + "\" if map file " + path);
+                System.exit(0);
+            }
+        }
+
+        file.close();
+    }
+
+    private void loadTriangle(Scanner file) {
+        
+    }
     
 }
