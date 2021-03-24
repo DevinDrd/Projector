7fcdcfb23a0609ecf3a645425f4a302820e395c8
DevinDrd <ddarnel8@msudenver.edu>
2021-03-24 17:02:41 -0600

    works?

--- travis-build.sh
+++ travis-build.sh
@@ -25,9 +25,13 @@ mv lib lwjgl
 mkdir junit
 cd junit
 
-curl https://search.maven.org/search?q=g:junit%20AND%20a:junit --output junit.jar
-curl https://search.maven.org/artifact/org.hamcrest/hamcrest-core/1.3/jar --output hamcrest-core.jar
+curl https://search.maven.org/remotecontent?filepath=junit/junit/4.13.2/junit-4.13.2.jar --output junit.jar
+curl https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar --output hamcrest-core.jar
 
 cd ../..
+pwd
+ls lib
+ls lib/junit
 
 javac -cp .:./src:./test:./lib/lwjgl/lwjgl.jar:./lib/lwjgl/lwjgl-glfw.jar:./lib/lwjgl/lwjgl-opengl.jar:./lib/junit/junit.jar:./lib/junit/hamcrest-core.jar -d ./bin test/TestRunner.java
+java -cp ./bin:./lib/lwjgl/lwjgl.jar:./lib/lwjgl/lwjgl-glfw.jar:./lib/lwjgl/lwjgl-glfw-natives-linux.jar:./lib/lwjgl/lwjgl-natives-linux.jar:./lib/lwjgl/lwjgl-opengl.jar:./lib/lwjgl/lwjgl-opengl-natives-linux.jar:./lib/junit/junit.jar:./lib/junit/hamcrest-core.jar -Djava.library.path=./lib/lwjgl TestRunner
